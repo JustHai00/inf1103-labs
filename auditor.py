313b1ab89de9b24b25da01f321ad7d75000c70de
JustHai00 <sinhuhai@gmail.com>
2026-09-11 16:51:46 +0800

    first

--- auditor.py
+++ auditor.py
@@ -2,3 +2,7 @@
 total_inventory = 0 # start from 0
 failed_inventory = 0 # have not started yet
 Max_Item= 500 # maximum number of items allowed in inventory
+
+print("Smart Inventory Auditor")
+print( "Enter stock quantities one at a time, or 'quit' to exit.\n")
+
