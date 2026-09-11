338f2738df56e2768f66c9f724baf100dc399472
JustHai00 <sinhuhai@gmail.com>
2026-09-11 16:53:34 +0800

    second

--- auditor.py
+++ auditor.py
@@ -6,3 +6,11 @@ Max_Item= 500 # maximum number of items allowed in inventory
 print("Smart Inventory Auditor")
 print( "Enter stock quantities one at a time, or 'quit' to exit.\n")
 
+# 2. continuous loop
+while True:
+    user_input = input("Enter stock quantity: ")
+
+    #exit condition
+    if user_input.lower() == 'quit':
+        break
+    
